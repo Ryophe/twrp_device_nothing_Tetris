0214b83dad9612f5083f0154e54cd53fb197063c
AndrexYT <andrexyt2012@gmail.com>
2024-12-03 15:45:03 +0100

    Tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2018-2021 PixysOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/potato_Tetris.mk
+
+COMMON_LUNCH_CHOICES := \
+    potato_Tetris-eng \
+    potato_Tetris-userdebug \
+    potato_Tetris-user
