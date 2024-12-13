443559812cae9182d542dabc7cc3b21a20515d9d
rawnullbyte <120054040+rawnullbyte@users.noreply.github.com>
2024-12-13 22:40:40 +0100

    Update Android.mk
    Fix of:
    FAILED: ninja: '/root/fox_12.1/out/target/product/Tetris/dtb.img', needed by '/root/fox_12.1/out/target/product/Tetris/vendor_boot.img', missing and no known rule to make it
    18:44:53 ninja failed with: exit status 1

--- Android.mk
+++ Android.mk
@@ -1,5 +1,13 @@
-
 LOCAL_PATH := $(call my-dir)
+
 ifeq ($(TARGET_DEVICE),Tetris)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+# Include the prebuilt dtb.img (Fix of the "missing and no known rule to make it" error)
+include $(CLEAR_VARS)
+LOCAL_MODULE := dtb.img
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := $(TARGET_PREBUILT_DTB)
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)
+include $(BUILD_PREBUILT)
 endif
