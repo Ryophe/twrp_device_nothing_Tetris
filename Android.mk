c84768fe31bf7389c97fbcbd63b198887990b15b
rawnullbyte <120054040+rawnullbyte@users.noreply.github.com>
2024-12-16 15:07:51 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,16 +2,4 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),Tetris)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
-
-# Define source files for DTB
-DTB_OBJS := \
-    $(wildcard $(BOARD_PREBUILT_DTB_DIR)/*.dtb)
-
-# Define the target for dtb.img
-$(PRODUCT_OUT)/dtb.img: $(DTB_OBJS)
-    @echo "Building dtb.img"
-    $(hide) cat $(DTB_OBJS) > $(PRODUCT_OUT)/dtb.img
-
-# Ensure dtb.img is a dependency of vendor_boot.img
-$(PRODUCT_OUT)/vendor_boot.img: $(PRODUCT_OUT)/dtb.img
 endif
